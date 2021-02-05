c1f4d528ba69fe45d85b6adbef2f02842b6ad87d
Franco Traverso <franco-t20@outlook.com>
2021-02-05 19:16:33 -0300

    added weather description

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=1df8c2e8ceafb40ac2f820b3298afeb3&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=1df8c2e8ceafb40ac2f820b3298afeb3&query=37.8267,-122.4233&units=f'
 
 request({ url: url, json: true}, (error, response) => {
 
     const temperature = response.body.current.temperature
     const aparentTemperature = response.body.current.feelslike
+    const weatherDescription = response.body.current.weather_descriptions[0]
 
-    console.log(`It is currently ${temperature} degrees out. It feels like ${aparentTemperature} degrees out.`)
+    console.log(`${weatherDescription}: It is currently ${temperature} degrees out. It feels like ${aparentTemperature} degrees out.`)
 })
