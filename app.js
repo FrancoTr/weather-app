b43c25bb105639aff527620ec816bb29524c4828
Franco Traverso <franco-t20@outlook.com>
2021-02-05 18:38:30 -0300

    customized URL request

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=1df8c2e8ceafb40ac2f820b3298afeb3&query=37.8267,-122.4233'
 
-request({ url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true}, (error, response) => {
+    console.log(response.body.current)
 })
