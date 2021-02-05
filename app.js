74edf6cf07202e065edf7ffd1c643ca8b176d71c
Franco Traverso <franco-t20@outlook.com>
2021-02-05 19:07:58 -0300

    more specific data request (temperature)

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=1df8c2e8ceafb40ac2f820b3298afeb3&query=37.8267,-122.4233'
 
 request({ url: url, json: true}, (error, response) => {
-    console.log(response.body.current)
+
+    const temperature = response.body.current.temperature
+    const aparentTemperature = response.body.current.feelslike
+
+    console.log(`It is currently ${temperature} degrees out. It feels like ${aparentTemperature} degrees out.`)
 })
