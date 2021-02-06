9b275bb1e69aa97baef5a31b40b06df4901a7de0
Franco Traverso <franco-t20@outlook.com>
2021-02-05 23:44:59 -0300

    geolocation added (through mapbox API)

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ request({ url: url, json: true}, (error, response) => {
 
     console.log(`${weatherDescription}: It is currently ${temperature} degrees out. It feels like ${aparentTemperature} degrees out.`)
 })
+
+// Geocoding
+//PUBLIC TOKEN: pk.eyJ1IjoiZnJhbmNvdHIiLCJhIjoiY2trc3VxNzluMW9sNDJvbnZtenFwbDA1NSJ9.HSp8UxneFQJmH5SUZ6VYZg
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZnJhbmNvdHIiLCJhIjoiY2trc3VxNzluMW9sNDJvbnZtenFwbDA1NSJ9.HSp8UxneFQJmH5SUZ6VYZg&limit=1'
+
+request({url: geocodeURL, json: true}, (error, response) => {   
+
+    const longitude = response.body.features[0].center[0]    //center[0] has the longitude coordinate
+    const latitude = response.body.features[0].center[1]     //center[1] has the latitude coordinate
+
+    console.log(`Latitude: ${latitude} ; Longitude: ${longitude}`)
+})
