29995646d9660496e74b3202941e760ef15f3d0c
Franco Traverso <franco-t20@outlook.com>
2021-02-11 22:22:36 -0300

    address location given through console by the user

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const request = require("request")
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+//Location address input
+const address = process.argv[2]
 
-geocode('Chicago', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
-    
-    forecast(data.latitude, data.longitude, (error, data) => {
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
         if (error) {
             return console.log(error)
         }
-        console.log('Error', error)
-        console.log('Data', data)
-      })
-})
+        
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
+
 
