dd5d435c698eb1d7267bb15f92af97dead5b39e0
Franco Traverso <franco-t20@outlook.com>
2021-02-11 21:45:09 -0300

    Callback chaining geocode into forecast

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const forecast = require('./utils/forecast')
 geocode('Chicago', (error, data) => {
     console.log('Error', error)
     console.log('Data', data)
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error', error)
+        console.log('Data', data)
+      })
 })
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
