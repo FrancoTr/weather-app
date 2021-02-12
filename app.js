eb3c8db3cd03c3d2e5723652c5d7599252b50f1b
Franco Traverso <franco-t20@outlook.com>
2021-02-11 21:50:59 -0300

    Error handling inside geocode & forecast callbacks

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const forecast = require('./utils/forecast')
 
 
 geocode('Chicago', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+    if (error) {
+        return console.log(error)
+    }
+    
     forecast(data.latitude, data.longitude, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
         console.log('Error', error)
         console.log('Data', data)
       })
