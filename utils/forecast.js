b6072037686457d612c0cb157c22bd63f6b6779b
Franco Traverso <franco-t20@outlook.com>
2021-02-12 23:23:28 -0300

    refactoring with property shorthand and object destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,15 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=1df8c2e8ceafb40ac2f820b3298afeb3&query=' + latitude + ',' + longitude + '&units=f'
 
-    request({url: url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            const temperature = response.body.current.temperature
-            const aparentTemperature = response.body.current.feelslike
-            const weatherDescription = response.body.current.weather_descriptions[0]
+            const temperature = body.current.temperature
+            const aparentTemperature = body.current.feelslike
+            const weatherDescription = body.current.weather_descriptions[0]
             callback(undefined, `${weatherDescription}: It is currently ${temperature} degrees out. It feels like ${aparentTemperature} degrees out.`)
         }
     })
