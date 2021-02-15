ab027785296b1e4b23063f809c7f36e02f013f49
Franco Traverso <franco-t20@outlook.com>
2021-02-14 23:20:21 -0300

    first draft HTML & JSON that will be send

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,25 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {         // root
-    res.send('Hello express!')
+    res.send('<h1>Weather App: Under construction</h1>')
 })
 
 app.get('/help', (req, res) => {    // ../help
-    res.send('Help page')
+    res.send({
+        name: 'Franco',
+        age: 31
+    })
 })
 
 app.get('/about', (req, res) => {   // ../about
-    res.send('About page')
+    res.send('<h1>About page: Under construction</h1>')
 })
 
 app.get('/weather', (req, res) => { // ../weather
-    res.send('Current weather')
+    res.send({
+        location: 'location TBD',
+        weather: 'weather TBD'
+    })
 })
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
