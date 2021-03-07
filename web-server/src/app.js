6174e1d9c93cb624c262fc03e9d0b5b02d1e924d
Franco Traverso <franco-t20@outlook.com>
2021-03-07 15:33:11 -0300

    JSON HTTP Endpoint with geocode & weather

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -46,11 +48,20 @@ app.get('/weather', (req, res) => { // ../weather
             error: 'You must provide an Address'
         })
     }
-    console.log(req.query.address)
-    res.send({
-        location: 'location TBD',
-        weather: 'weather TBD',
-        address: req.query.address
+    geocode(req.query.adress, (error, {latitude, longitude, location}) => {
+        if (error) {
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
