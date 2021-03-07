3827cfc032932367df08eb74ea7d145897a7f86e
Franco Traverso <franco-t20@outlook.com>
2021-03-07 15:59:43 -0300

    fixed a typo in the get query

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -48,7 +48,7 @@ app.get('/weather', (req, res) => { // ../weather
             error: 'You must provide an Address'
         })
     }
-    geocode(req.query.adress, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
         if (error) {
             return res.send({ error })
         }
